fbba3906a2d82e13510dd1e4220d1a888e199879
Harriet Bull <email>
2019-03-18 19:46:00 +0000

    Signup form now works, need to fix output

--- main.py
+++ main.py
@@ -7,20 +7,25 @@ app = Flask("__name__")
 def index():
     return render_template("index.html")
 
+@app.route("/ad")
+def sign_up():
+    return render_template("signUp.html")
+
 # @app.route("/signUp")
 # def signUp():
 #     return render_template("signUp.html")
 
-@app.route("/signUp", methods=["POST"])
-def signUp():
+@app.route("/output", methods=["POST"])
+def sUp():
     # read the posted values from the UI
-    name = request.form['inputName']
-    email = request.form['inputEmail']
-    password = request.form['inputPassword']
+    name = request.form['your_name']
+    email = request.form['your_email']
 
-    # validate the received values
-    if name and email and password:
-        return "You are signed up"
+    #
+    # # validate the received values
+    if name and email:
+        return "Yes it works"
+
 
 if  __name__ == "__main__":
     app.run(debug=True)
