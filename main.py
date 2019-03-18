51492d63030bcf88dabb4268ab159b712999e7e0
Harriet Bull <email>
2019-03-18 20:06:33 +0000

    making forms work

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import requests
 
 app = Flask("__name__")
 
+def send_simple_message(email, name):
+    return requests.post(
+        "https://api.mailgun.net/v3/sandboxc42735b98d7b464587d8a7396a7e7741.mailgun.org/messages",
+        auth=("api", "5bb876706f10a10b2f2e5a3d12804f39-7caa9475-a3d1e7df"),
+        data={"from": "Excited User <mailgun@sandboxc42735b98d7b464587d8a7396a7e7741.mailgun.org>",
+              "to": [email],
+              "subject": "Thanks for signing up "+name,
+              "text": "You've signed up to receive more healthy updates - well done you!"})
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -24,7 +33,8 @@ def sUp():
     #
     # # validate the received values
     if name and email:
-        return "Yes it works"
+        send_simple_message(email, name)
+        return render_template("thanks.html")
 
 
 if  __name__ == "__main__":
