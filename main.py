f98cbae92da4bf0f9870466ec55611bd8a7961ab
afsh94 <f.mahmood@qmul.ac.uk>
2019-03-21 12:46:29 +0000

    Python project files Version3

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 
 app = Flask("__name__")
 
+# Using Mailgun to send emails to subscribers
 def send_simple_message(email, name):
     return requests.post(
         "https://api.mailgun.net/v3/sandboxc42735b98d7b464587d8a7396a7e7741.mailgun.org/messages",
@@ -12,34 +13,44 @@ def send_simple_message(email, name):
               "subject": "Thanks for signing up "+name,
               "text": "You've signed up to receive more healthy updates - well done you!"})
 
+# Link to Home page
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# Link to Sign up page for subscription
 @app.route("/ad")
 def sign_up():
     return render_template("signUp.html")
 
-@app.route("/bmi")
-def bmi_page():
-    return render_template("bmi.html")
-
-# @app.route("/signUp")
-# def signUp():
-#     return render_template("signUp.html")
-
 @app.route("/output", methods=["POST"])
 def sUp():
-    # read the posted values from the UI
+# read the posted values from the UI
     name = request.form['your_name']
     email = request.form['your_email']
 
-    #
-    # # validate the received values
+# validate the received values
     if name and email:
         send_simple_message(email, name)
         return render_template("thanks.html")
 
+# Link to BMI input page for weight and height
+@app.route("/bmi")
+def bmi_page():
+    return render_template("bmi.html")
 
+# Link to BMI calculation page
+@app.route("/bmiCalc", methods=["POST"])
+def calc():
+# read the posted values from the UI
+    weight = float(request.form['your_weight'])
+    height = float(request.form['your_height'])
+
+# Calculate BMI using values submitted
+    if weight and height:
+        bmi = weight / (height*height)
+        return render_template("bmiResult.html", bmi=round(bmi,2))
+
+    
 if  __name__ == "__main__":
     app.run(debug=True)
